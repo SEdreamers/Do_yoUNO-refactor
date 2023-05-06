ef1706f1206e79332ec0442dbbcbca63e434a957
Seol'Lab <83863024+SeolLab@users.noreply.github.com>
2023-05-06 13:57:24 +0900

    Add files via upload

---
+++ UNO/scenes/lobby.py
@@ -0,0 +1,11 @@
+from engine.scene import Scene
+
+class Lobby(Scene):
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+    
+    def draw(self):
+        pass
---
+++ UNO/scenes/multi.py
@@ -0,0 +1,12 @@
+from engine.scene import Scene
+
+class Lobby(Scene):
+    def __init__(self):
+        pass
+    
+    @override
+    def run(self):
+        pass
+    
+    def draw(self):
+        pass
