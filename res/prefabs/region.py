f17fd52de3a5af4eddd3bc607f510d2e9a27b2a7
cshooon <0015chseang@gmail.com>
2023-05-08 17:23:43 +0900

    feat: region 파일 추가

---
+++ res/prefabs/region.py
@@ -0,0 +1,11 @@
+from engine.game_object import GameObject
+
+
+class deck_space(GameObject):
+    def __init__(self):
+        pass
+
+
+class hand_space(GameObject):
+    def __init__(self):
+        pass
